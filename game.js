2adbaf757c9399d0045dae04164b6648234c41f3
Marek Witkowski <marek.witkowski@onet.com.pl>
2020-08-05 11:27:51 +0200

    game loop done

---
+++ game.js
@@ -0,0 +1,30 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+let lastRenderTime = 0;
+
+//game loop
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  //ms to seconds
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  //snake speed, 2 times / second
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake();
+}
