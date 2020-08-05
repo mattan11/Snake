16b692ab9c8b60ec45b035322321347ea49fbcdb
Marek Witkowski <marek.witkowski@onet.com.pl>
2020-08-05 12:58:30 +0200

    add lose conditions

--- game.js
+++ game.js
@@ -2,15 +2,25 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
 } from "./snake.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 //game loop
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart")) {
+      window.location = "/";
+    }
+    return;
+  }
   window.requestAnimationFrame(main);
   //ms to seconds
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
@@ -28,6 +38,7 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
@@ -35,4 +46,8 @@ function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
