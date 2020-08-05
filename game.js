10940dd3c964785a384f8758457d11b1916d8477
Marek Witkowski <52462935+mattan11@users.noreply.github.com>
2020-08-05 13:02:28 +0200

    update gh page location

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ const gameBoard = document.getElementById("game-board");
 function main(currentTime) {
   if (gameOver) {
     if (confirm("You lost. Press ok to restart")) {
-      window.location = "/";
+      window.location = "https://mattan11.github.io/Snake/";
     }
     return;
   }
